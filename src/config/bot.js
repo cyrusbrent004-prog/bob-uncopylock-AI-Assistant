8fe5affe526e80283b96bd0b71cc3dccedca5684
cyrusbrent004-prog <cyrusbrent004@gmail.com>
2026-09-13 10:12:29 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -94,7 +94,7 @@ export const botConfig = {
     colors: {
       // Main brand colors.
       primary: "#336699",
-      secondary: "#2F3136",
+      secondary: "##0000FF",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
